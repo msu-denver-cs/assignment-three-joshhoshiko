15509c314d8c0d5268cd61b70abc046990c1fbe7
joshhoshiko <jhoshiko@msudenver.edu>
2019-10-29 20:20:41 -0600

    Submiting all up to date code

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "cars#index"
+
+  resources :parts do
+    collection do
+      get 'search'
+    end
+  end
+  resources :makes do
+    collection do
+      get 'search'
+    end
+  end
+  resources :cars do
+    collection do
+      get 'search'
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
